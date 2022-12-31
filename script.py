3e9fe9573b07f54ddff250746dcbb5e3e24c2840
Malachi Soord <inverse.chi@gmail.com>
2022-12-31 15:48:13 +0100

    Fix lint

--- script.py
+++ script.py
@@ -1,8 +1,7 @@
-from edge_addons_api.client import Options, Client
-
 import os
 import sys
 
+from edge_addons_api.client import Client, Options
 
 if len(sys.argv) < 3:
     print("You must provide file_path and notes")
@@ -15,12 +14,9 @@ options = Options(
     product_id=os.environ["EDGE_PRODUCT_ID"],
     client_id=os.environ["EDGE_CLIENT_ID"],
     client_secret=os.environ["EDGE_CLIENT_SECRET"],
-    access_token_url=os.environ["EDGE_ACCESS_TOKEN_URL"]
+    access_token_url=os.environ["EDGE_ACCESS_TOKEN_URL"],
 )
 
 client = Client(options)
 
-client.submit(
-    file_path=file_path,
-    notes=notes
-)
+client.submit(file_path=file_path, notes=notes)
