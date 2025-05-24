2dfcb23608c6f1980baeaa6b6e47686f5e10423e
Malachi Soord <inverse.chi@gmail.com>
2025-05-25 01:50:14 +0200

    Fix test

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,6 @@ from edge_addons_api.client import Client, Options
 
 class ClientTest(TestCase):
     def test_submit_missing_file(self):
-        client = Client(Options("", "", "", ""))
-
+        client = Client(Options("", "", ""))
         with self.assertRaises(FileNotFoundError):
             client.submit("/non/existent/path", "new version upload")
